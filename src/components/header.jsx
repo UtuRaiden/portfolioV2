8ee56869879448ee2e10800e4dd7a9090c23a511
unknown <barronpeterson56@gmail.com>
2023-08-21 19:42:53 -0500

    bug fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div>
             <nav className='flex justify-end mb-10 mr-5'>
                 <ul>
-                    <Link to="portfolioV2/" className='mr-5 text-teal-500'>About</Link>
+                    <Link to="/" className='mr-5 text-teal-500'>About</Link>
                     <Link to="portfolioV2/project" className='mr-5 text-teal-500'>Projects</Link>
                     <Link to="portfolioV2/contact" className='mr-5 text-teal-500'>Contact</Link>
                     <Link to="portfolioV2/resume" className='text-teal-500'>Resume</Link>
